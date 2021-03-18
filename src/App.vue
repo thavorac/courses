ee402b36edddd9effa9898fe55e64d4e1fb2579d
Thavorac <thavorac.chun@gmail.com>
2021-03-18 16:36:53 +0700

    init project with simple set up

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,20 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <HelloWorld />
+  <ChildComponent />
 </template>
 
 <script>
 import HelloWorld from './components/HelloWorld.vue'
+import ChildComponent from './components/ChildComponent.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    HelloWorld,
+    ChildComponent
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
 </style>
---
+++ src/components/ChildComponent.vue
@@ -0,0 +1,12 @@
+<template>
+  <div>
+    This is new child component
+  </div>  
+</template>
+
+<script>
+export default {}
+</script>
+
+<style>
+</style>
