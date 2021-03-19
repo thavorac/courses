41a5849233da02f5866d464c1c1406f10cfa67a6
Thavorac <thavorac.chun@gmail.com>
2021-03-19 15:40:30 +0700

    Init vuejs cli project and add myButton component

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,14 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <MyButton></MyButton>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import MyButton from './components/MyButton.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    MyButton
   }
 }
 </script>
---
+++ src/components/MyButton.vue
@@ -0,0 +1,34 @@
+<template>
+  <button :class="state" v-on:click="clickButton">
+    <span v-if="state == 'delete'">Delete</span>
+    <span v-else>Add</span>
+  </button>
+</template>
+
+<script>
+export default {
+  name: "MyButton",
+  data() {
+    return {
+      state: 'delete'  // delete or add
+    }
+  },
+  methods: {
+    clickButton() {
+      console.log('button is clicked')
+    }
+  }
+}
+</script>
+
+<style scoped>
+  button {
+    border: 1px solid black;
+  }
+  .delete {
+    background-color: red;
+  }
+  .add {
+    background-color: violet;
+  }
+</style>
