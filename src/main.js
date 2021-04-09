35e2baf5a0d211f8731f79346733aee52cb3cfbb
Thavorac <thavorac.chun@gmail.com>
2021-04-09 15:25:51 +0700

    [TAK] Exercise 1

--- src/main.js
+++ src/main.js
@@ -1,4 +1,23 @@
 import { createApp } from 'vue'
+import { createStore } from 'vuex'
 import App from './App.vue'
 
-createApp(App).mount('#app')
+// Create vuex store
+const store = createStore({
+  state() {
+    return {}
+  },
+  mutations: {
+
+  },
+  actions: {
+
+  },
+  getters: {
+
+  }
+})
+
+const app = createApp(App)  // Create new vuejs instance
+app.use(store)              // Attach our new store to this vuejs instance
+app.mount('#app')           // mount vuejs instance with template
